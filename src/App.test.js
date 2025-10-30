20756e519bd0461d832f2f7d1306fab6c3c313f9
yasirakgn <ysrakhnn@gmail.com>
2025-10-30 16:44:55 +0300

    v3.0

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders automation control center header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/OTOMASYON KONTROL MERKEZİ/i);
+  expect(headerElement).toBeInTheDocument();
 });
+
