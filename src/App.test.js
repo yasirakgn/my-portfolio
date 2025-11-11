a920e6d8a5bf5136d10f4303c6a618cb102e25d4
yasirakgn <ysrakhnn@gmail.com>
2025-11-12 02:00:39 +0300

    açıklama

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders automation control center header', () => {
-  render(<App />);
-  const headerElement = screen.getByText(/OTOMASYON KONTROL MERKEZİ/i);
-  expect(headerElement).toBeInTheDocument();
-});
-
